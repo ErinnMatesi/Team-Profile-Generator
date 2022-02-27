b2bd851f752792aa8b01796c148a50aa24000e66
Faerovia <94494761+ErinnMatesi@users.noreply.github.com>
2022-02-27 15:44:00 -0800

    created generateLI function to create final list item in Employee cards, updated styling

---
+++ src/generateHTML.js
@@ -0,0 +1,21 @@
+function generateHTML(data) {
+    return ''
+};
+
+// create li with employeeType details and append
+function generateLI(data) {
+    if (data.employeeType === 'Manager') {
+        var li = document.getElementById('#type');
+        li.innerHTML =`Office number: ${data.office}`;
+    } else if (data.employeeType === 'Engineer') {
+        var li = document.getElementById('#list');
+        li.innerHTML = `GitHub: <a href="https://github.com/${data.github}">${data.github}</a>`;
+    } else if (data.employeeType === 'Intern') {
+        var li = document.getElementById('#list');
+        li.innerHTML =`School: ${data.school}`;
+    }
+};
+
+function generateCards(data) {
+    // generate a card for each team member
+};
